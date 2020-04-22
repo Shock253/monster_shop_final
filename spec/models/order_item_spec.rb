00dcb09f7e3228d785337ee17e20bc98fac809f6
Ethan Hocking <eshocking253@gmail.com>
2020-04-21 23:41:22 -0600

    Fix order_item_4 to have an item from brian

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -13,13 +13,22 @@ RSpec.describe OrderItem do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+      @gremlin = @brian.items.create!(
+        name: 'Gremlin',
+        description: "Don't feed me after midnight!",
+        price: 50,
+        image: 'https://cnet3.cbsistatic.com/img/WNE8g6NQlu1ZYaeP3wQAQPyZbUI=/1200x675/2017/05/11/b5c80061-817c-47e1-ade2-d54d57b71c0b/poll80srebootgremlins.jpg',
+        active: true,
+        inventory: 20)
+
       @user = User.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
       @order_1 = @user.orders.create!
       @order_2 = @user.orders.create!
       @order_item_1 = @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
       @order_item_2 = @order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 3)
       @order_item_3 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 15)
-      @order_item_4 = @order_2.order_items.create!(item: @giant, price: @giant.price, quantity: 27)
+      @order_item_4 = @order_2.order_items.create!(item: @gremlin, price: @gremlin.price, quantity: 27)
     end
 
     it '.subtotal' do
